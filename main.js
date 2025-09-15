b74e05c5a0d0e7ec424a91b97c0409756dd6c4ac
9maple <emmarpark@gmail.com>
2025-09-15 19:30:34 +0000

    added some SICK buttons

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const studentButton = document.getElementById("studentBtn");
 const teacherButton = document.getElementById("teacherBtn");
+const  homeButtom= document.getElementById("homeBtn");
 
 studentButton.addEventListener("click", () => {
   window.location.href = "studentLogin.html";
